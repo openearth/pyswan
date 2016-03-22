0d759e0843a0e59216e85535810c388722f0228a
Gerben J. de Boer <gerbenjdeboer@gmail.com>
2016-03-22 08:52:20 +0100

    started swan package with seperate generic metocean package

---
+++ oceanwaves.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Module for handling spectral ocean wave data.
+  
+"""
+
+"""
+$HeadURL:  $
+$Id: m $
+"""
+__version__ = "$Revision: $"
+
+#  Copyright notice
+#   --------------------------------------------------------------------
+#   Copyright (C) 2016 TU Delft/Van Oord
+#       Gerben J. de Boer, <g.j.deboer@tudelft.nl>/<gerben.deboer@vanoord.com>
+#
+#   This library is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+#   --------------------------------------------------------------------
+
+# This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+# OpenEarthTools is an online collaboration to share and manage data and
+# programming tools in an open source, version controlled environment.
+# Sign up to recieve regular updates of this function, and to contribute
+# your own tools.
+
+import numpy as np
+import datetime
+debug = False
+   
+class Spec2():
+    """
+    Class for 2D spectra. Array shape is temporal series for a list of points.
+    """
+    
+    def __init__(self):
+        
+        self.buoy            = []
+        self.sensor          = []
+        self.source          = ''
+        self.t               = []
+        self.x               = []
+        self.y               = []
+        self.lon             = []
+        self.lat             = []
+        self.epsg            = [] # epsg for (x,y) <-> (lon,lat)
+        self.text            = []
+        self.version         = []
+        
+        self.f               = [] # always implicit Hz
+        self.ftype           = []
+        
+        self.direction       = []
+        self.direction_units = '' # implicit directiontype: degrees_north or degrees_true        
+        
+        self.energy          = np.asarray([[[[]]]]) # [t,xy,f,dir] # last dimension 'dir' extra wrt Spec1
+        self.energy_units    = ''
+
+        #self.__dict__.update(kwargs)    
+        
+    def __repr__(self):
+
+        return '<Spectrum2D ' + ' shape:[nt:' + str(self.energy.shape[0])+ \
+        ',nx:'+str(self.energy.shape[1])+ ',nf:'+str(self.energy.shape[2])+ \
+        ',nd:'+str(self.energy.shape[3]) + ']: "'+self.source+'">'
+        
+   #def Tmij(self):
+   #TO DO calculate period based on various spectral moments
+    
+    def Hm0(self):
+        """
+        Integrate Hm0 (Hs) from wave spectra (using trapz rule along last dimension.)
+        S = swan.Spec2()
+        S.Hm0()
+        """
+    
+        if self.energy_units[0:9] == 'm2/Hz/deg': # deg, degr, degree
+            # np.abs: descending directions lead to negs
+            m0 = np.trapz(np.abs(np.trapz(self.energy,self.direction)),self.f, axis=-1) # int along last dimension
+        else:
+            m0 = None
+            print('unknown units:"',self.energy_units,'"')
+        
+        return 4*np.sqrt(m0)
+        
+    #@static
+    def plot(self,fname,it=0,ix=0):        
+        """
+        """ 
+        
+        import matplotlib.pyplot as plt
+        fig=plt.figure()
+        fig.set_figwidth(10)
+        ax = plt.axes([0.15,.15,0.8,0.8])
+        ax.set_xlabel('direction')
+        ax.set_ylabel('f')
+        plt.pcolormesh(self.direction,self.f,self.energy[it,ix,:,:])        
+        ax.set_title('[' + str(self.lat) + ','+ str(self.lon) + ']')
+        plt.savefig(fname, fontsize=7, dpi=400)
+        plt.close()             
+
+class Spec1():
+    """
+    Class for 1D spectra. Array shape is temporal series for a list of points.
+    """
+    
+    def __init__(self):
+        
+        self.buoy            = []
+        self.sensor          = []
+        self.source          = ''
+        self.t               = []
+        self.x               = []
+        self.y               = []
+        self.lon             = []
+        self.lat             = []
+        self.epsg            = [] # epsg for (x,y) <-> (lon,lat)
+        self.text            = []
+        self.version         = []
+        
+        self.f               = [] # always Hz
+        self.ftype           = []
+        
+        self.direction       = np.asarray([[[]]]) # [t,xy,f]
+        self.direction_units = '' # implicit directiontype: degrees_north or degrees_true
+        
+        self.energy          = np.asarray([[[]]]) # [t,xy,f] # last dimension 'f' extra wrt Spec1
+        self.energy_units    = ''
+        
+        self.spreading       = np.asarray([[[]]]) # [t,xy,f]
+        self.spreading_units = ''
+        
+        #self.__dict__.update(kwargs) 
+        
+    def __repr__(self):
+        
+        return '<Spectrum1D ' + ' shape:[nt:' + str(self.energy.shape[0])+ ',nx:'+str(self.energy.shape[1])+ ',nf:'+str(self.energy.shape[2]) + ']: "'+self.source+'">'
+        
+   #def Tmij(self):
+   #TO DO calculate period based on various spectral moments
+   
+    def from_jonswap(self,Hm0,Tp,
+        g         = 9.81,
+        gamma     = 3.3, 
+        method    = 'Yamaguchi', 
+        normalize = True,
+        sa        = 0.07,
+        sb        = 0.09):
+        """
+        Generate JONSWAP spectrum
+        S = swan.Spec1(Hm0,Tp)
+        
+        """
+        
+        f = self.f
+        
+        # method    = 'Yamaguchi'; # 'Goda'        
+
+        def sigma(f,fpeak,sa,sb):
+            s = np.ones(f.size)*sa
+            s[f > fpeak] = sb
+            return s
+
+        if method=='Yamaguchi':
+            alpha = 1/(0.06533*gamma**0.8015 + 0.13467)/16; # Yamaguchi (1984), used in SWAN
+        elif method=='Goda':
+            alpha = 1/(0.23+0.03*gamma-0.185*(1.9+gamma)**-1)/16; # Goda
+
+        pm  = alpha*Hm0**2*Tp**-4*f**-5*np.exp(-1.25*(Tp*f)**-4);
+        E = pm*gamma**np.exp(-0.5*(Tp*f-1)**2./sigma(f,1/Tp,sa,sb)**2);
+
+        if normalize:
+            corr = Hm0**2/(16*np.trapz(E,f))
+            E = E*corr
+            
+        self.energy = E
+        self.energy_units = 'm2/Hz'  
+
+        return self.energy
+
+
+    def Hm0(self):
+        """
+        Integrate Hm0 (Hs) from wave spectra (using trapz rule along last dimension.)
+        S = swan.Spec1()
+        S.Hm0()
+        """
+    
+        if self.energy_units == 'm2/Hz':
+            m0 = np.trapz(self.energy,self.f, axis=-1) # int along last dimension
+        else:
+            m0 = None
+            print('unknown units',self.energy_units)
+        
+        return 4*np.sqrt(m0)
+
+    #@static
+    def plot(self,fname,it=0,ix=0):        
+        """
+        """ 
+        
+        import matplotlib.pyplot as plt
+        fig=plt.figure()
+        fig.set_figwidth(10)
+        ax = plt.axes([0.15,.15,0.8,0.8])
+        plt.plot(self.f,self.energy[it,ix,:])
+        ax.set_xlabel('f [Hz]')
+        ax.set_ylabel('E [' + self.energy_units + ']')
+        ax.set_title('[' + str(self.lat) + ','+ str(self.lon) + ']')
+        
+        ax.set_xlim(0,np.max(self.f))
+        plt.savefig(fname, fontsize=7, dpi=400)
+        plt.close()          
+
+        
+class Spec0():
+    """
+    Class for spectral parameters
+    """
+
+    def __init__(self):
+        
+        self.buoy            = []
+        self.sensor          = []
+        self.source          = ''
+        self.t               = []
+        self.x               = []
+        self.y               = []
+        self.lon             = []
+        self.lat             = []
+        self.epsg            = [] # epsg for (x,y) <-> (lon,lat)
+        self.text            = []
+        self.version         = []
+        
+        self.Hmax            = np.asarray([[]])
+        self.Hs              = np.asarray([[]])
+        self.Tp              = np.asarray([[]])
+        self.Tm01            = np.asarray([[]])
+        self.Tm02            = np.asarray([[]])
+
+        #self.__dict__.update(kwargs)     
+
+        
+    def __repr__(self):
+        
+        return '<Spectrum0D ' + ' shape:[nt:' + str(self.Hs.shape[0])+ \
+        ',nx:'+str(self.Hs.shape[1])+ ']: "'+self.source+'">'
+        
+    def from_Spec(self,Spec):
+    
+        self.buoy   = Spec.buoy   
+        self.sensor = Spec.sensor 
+        self.source = Spec.source 
+        self.t      = Spec.t      
+        self.x      = Spec.x      
+        self.y      = Spec.y      
+        self.lon    = Spec.lon    
+        self.lat    = Spec.lat    
+        self.epsg   = Spec.epsg   
+        self.text   = Spec.text   
+        
+        self.Hs     = Spec.Hm0()
+    
+        return self
---
+++ swan.py
@@ -0,0 +1,424 @@
+# -*- coding: utf-8 -*-
+"""
+Module for handling SWAN input/output
+  
+"""
+
+"""
+$HeadURL:  $
+$Id:  $
+"""
+__version__ = "$Revision: $"
+
+#  Copyright notice
+#   --------------------------------------------------------------------
+#   Copyright (C) 2016 TU Delft/Van Oord
+#       Gerben J. de Boer, <g.j.deboer@tudelft.nl>/<gerben.deboer@vanoord.com>
+#
+#   This library is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+#   --------------------------------------------------------------------
+
+# This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+# OpenEarthTools is an online collaboration to share and manage data and
+# programming tools in an open source, version controlled environment.
+# Sign up to recieve regular updates of this function, and to contribute
+# your own tools.
+
+import numpy as np
+import datetime
+import oceanwaves as spectrum
+debug = False
+   
+#@static
+def from_swan_header(f, source='from_swan'):
+    """
+    """
+    
+    self = {}
+    
+    self["source"] = source
+    self["text"] = []
+    self["version"] = f.readline().split()[1].strip()
+    self["text"].append(f.readline().split('$')[1])
+    self["text"].append(f.readline().split('$')[1])
+    
+    # when
+
+    t = f.readline()
+    if t.split()[0].strip() == 'TIME':
+        timecoding = int(f.readline().split()[0].strip())
+    else:
+        timecoding = None
+
+    # where
+
+    xy  = f.readline().split()[0].strip()
+    nxy = int(f.readline().split()[0])
+    self["x"]   = []
+    self["y"]   = []
+    self["lon"] = []
+    self["lat"] = []
+    
+    if debug:
+        print('POINTS')
+    for i in range(nxy):
+        raw = f.readline()
+        if debug:
+            print(i,': ',raw.rstrip())
+        if xy == 'LONLAT':
+            self["lon"].append(float(raw.split()[0]))
+            self["lat"].append(float(raw.split()[1]))
+            self["x"].append(None)
+            self["y"].append(None)
+        else:
+            self["x"].append(float(raw.split()[0]))
+            self["y"].append(float(raw.split()[1]))
+            self["lon"].append(None)
+            self["lat"].append(None)
+
+    # frequency
+
+    self["f"] = []
+    self["ftype"] = f.readline().split()[0].strip()
+    
+    if self["ftype"] == 'CDIR':
+        self["ftype"] = 'cartesian'
+    else: # NDIR
+        self["ftype"] = 'nautical'
+        
+    nf = int(f.readline().split()[0])
+    for i in range(nf):
+        raw = f.readline()
+        self["f"].append(float(raw.split()[0]))
+    self["f"] = np.asarray(self["f"])
+        
+    return self, timecoding
+
+def from_file2D(f,source='from_swan'):
+    """
+    Load 2D spectral series file from SWAN spectral file/buffer.
+    
+    Matrix 'energy' has 4 dimensions:
+    [time x locations x frequencies x directions]
+    
+    S = spectrum.Spec2()
+    f = fopen(r'c:\temp\myfile.spc')
+    S.from_swan(f,source='myfile.spc') # optionally remember source
+    f.close()
+    """           
+    self = spectrum.Spec2()
+    
+    D, timecoding = from_swan_header(f)
+    for k in D.keys():
+        self.__setattr__(k, D[k])    
+    self.source = source
+    
+    # direction
+
+    self.direction = []
+    direction_type = f.readline().split()[0].strip()
+    nf = int(f.readline().split()[0])
+    for i in range(nf):
+        raw = f.readline()
+        self.direction.append(float(raw.split()[0]))
+    self.direction = np.asarray(self.direction)
+    
+    if direction_type == 'CDIR':
+        self.direction_units = 'degrees_true'  # CF convention
+    else:    
+        self.direction_units = 'degrees_north' # CF convention
+    
+    # what
+    
+    f.readline() # QUANT
+    number_of_quantities = int(f.readline().split()[0]) # 1
+    quantity_names = []
+    quantity_units = []
+    quantity_exception_values = []
+    for j in range(number_of_quantities):
+        quantity_names.append(f.readline().split()[0].strip())
+        quantity_units.append(f.readline().split()[0].strip())
+        quantity_exception_values.append(float(f.readline().split()[0].strip()))
+    self.energy_units    = quantity_units[0]
+    
+    # data
+    
+    raw = 'ini'
+    En4 = []
+    if debug:
+        print('TIMES')
+    while not raw == '':
+        if not timecoding == None:
+            raw = f.readline()
+            if raw == '':
+                break
+            tstr = raw.split()[0].strip()
+            if timecoding==1:
+                self.t.append(datetime.datetime.strptime(tstr,'%Y%m%d.%H%M%S'))
+            else:
+                self.t.append(tstr)
+                print('timecoding unknown: ',timecoding)
+            if debug:
+                print(len(self.t),': ',self.t[-1])
+                
+            #print('UNDER CONSTRUCTION')
+            #return self
+        
+        En3 = []
+        for i in range(len(self.x)):
+            loc = f.readline().split()[0]
+            if loc =='FACTOR':
+                factor = float(f.readline().split()[0].strip())
+                if debug:
+                    print('   POINT ',i,' FACTOR:',factor)
+            else:
+                if debug:
+                    print('   POINT ',i,' FACTOR:',loc)
+                
+            En2 = []
+            for j in range(len(self.f)):
+                En1 = []
+                raw = f.readline()
+                if raw == '':
+                    break
+                else:
+                    for k in range(len(self.direction)):
+                        En1.append(float(raw.split()[k])*factor)
+                En2.append(En1)
+            En3.append(En2)
+        En4.append(En3)
+    En4 = np.asarray(En4)
+    self.t      = np.asarray(self.t)
+    self.energy = np.ma.masked_array(En4,En4==quantity_exception_values[0])
+    
+    return self 
+    
+#@static
+def from_file1D(f,source='from_swan'):
+    """
+    Load 1D spectral series file from SWAN spectral file/buffer.
+    
+    Matrices 'energy','direction','spreading' have 3 dimensions:
+    [time x locations x frequencies]
+    
+    S = spectrum.Spec1()
+    f = fopen(r'c:\temp\myfile.s1d')
+    S.from_file1D(f,source='myfile.s1d') # optionally remember source
+    f.close()
+    """   
+
+    self = spectrum.Spec1()
+    
+    D, timecoding = from_swan_header(f)
+    for k in D.keys():
+        self.__setattr__(k, D[k])
+    self.source = source
+    
+    # what
+
+    f.readline() # QUANT
+    number_of_quantities = int(f.readline().split()[0]) # 3
+    quantity_names = []
+    quantity_units = []
+    quantity_exception_values = []
+    for j in range(number_of_quantities):
+        quantity_names.append(f.readline().split()[0].strip())
+        quantity_units.append(f.readline().split()[0].strip())
+        quantity_exception_values.append(float(f.readline().split()[0].strip()))
+    self.energy_units    = quantity_units[0]
+    self.direction_units = quantity_units[1]
+    self.spreading_units = quantity_units[2]
+    if quantity_names[1] == 'CDIR':
+        self.direction_units = 'degrees_true'  # CF convention
+    else:    
+        self.direction_units = 'degrees_north' # CF convention
+    if quantity_names[2] == 'DSPRD' or quantity_names[2] == 'DEGR':
+        self.spreading_units = 'degrees' # DSPRD or DEGR
+    else:    
+        self.spreading_units = '1'       # DSPRP or POWER         
+        
+    # data
+
+    raw = 'ini'
+    En3,Th3,Sp3 = [],[],[]
+    if debug:
+        print('TIMES')
+    while not raw == '':
+        if not timecoding == None:
+            raw = f.readline()
+            if raw == '':
+                break
+            tstr = raw.split()[0].strip()
+            if timecoding==1:
+                self.t.append(datetime.datetime.strptime(tstr,'%Y%m%d.%H%M%S'))
+            else:
+                self.t.append(tstr)
+                print('timecoding unknown: ',timecoding)
+            if debug:
+                print(len(self.t),': ',self.t[-1])
+        En2,Th2,Sp2 = [],[],[]
+        for i in range(len(self.x)):
+            loc = f.readline()
+#  TO DO: can be NODATA
+            En1,Th1,Sp1 = [],[],[]
+            for j in range(len(self.f)):
+                raw = f.readline()
+                if raw == '':
+                    break
+                else:
+                    En1.append(float(raw.split()[0]))
+                    Th1.append(float(raw.split()[1]))
+                    Sp1.append(float(raw.split()[2]))
+            En2.append(En1)
+            Th2.append(Th1)
+            Sp2.append(Sp1)
+        En3.append(En2)
+        Th3.append(Th2)
+        Sp3.append(Sp2)
+    En3 = np.asarray(En3)
+    Th3 = np.asarray(Th3)
+    Sp3 = np.asarray(Sp3)
+    
+    self.t         = np.asarray(self.t)
+    self.energy    = np.ma.masked_array(En3,En3==quantity_exception_values[0])
+    self.direction = np.ma.masked_array(Th3,Th3==quantity_exception_values[1])
+    self.spreading = np.ma.masked_array(Sp3,Sp3==quantity_exception_values[2])
+    
+    return self
+    
+#@static
+def to_file1D(self,f):
+    """
+    Save 1D spectral series file to SWAN spectral file/buffer.
+    
+    S1 = spectrum.Spec1() 
+    # fill S1
+    f = fopen(r'c:\temp\newfile.s1d')
+    S1.to_file1D(f)
+    f.close()        
+    
+    """          
+    
+    f.writelines('SWAN ' + str(self.version) + '\n')
+    for t in self.text:
+        f.writelines('$' + t)
+        
+    # TIME    
+    if len(self.t)>0:
+        f.writelines('TIME\n')
+    f.writelines(str(len(self.x)) + '\n')
+
+    # LOC
+    print(self.lon[0])
+    print(self.x[0])
+    if self.x[0] == None:
+        f.writelines('LONLAT\n')
+        f.writelines(str(len(self.lon)) + '\n')
+        for i in range(len(self.lon)):
+            f.write("{:f} {:f}\n".format(self.lon[i],self.lat[i]))
+    elif self.lon[0] == None:
+        f.writelines('LOCATIONS\n')
+        f.writelines(str(len(self.x)) + '\n')
+        for i in range(len(self.x)):
+            f.write("{:f} {:f}\n".format(self.x[i],self.y[i]))                 
+        
+    # FREQ
+    if self.ftype == 'absolute':
+        f.writelines('AFREQ\n')
+    else:    
+        f.writelines('FREQ\n')
+    f.writelines(str(len(self.f))+ '\n')
+    for freq in self.f:
+        f.writelines("{:f}\n".format(freq))
+
+    # QUANT
+    f.writelines('QUANT \n')
+    f.writelines('3 \n')
+
+    f.writelines('VaDens \n')
+    f.writelines(self.energy_units + '\n')
+    f.writelines('NaN' + '\n')
+
+    if self.direction_units == 'degrees_true':
+        f.writelines('CDIR\n')
+    else:    
+        f.writelines('NDIR\n')
+    f.writelines(self.direction_units + '\n')
+    f.writelines('NaN' + '\n')
+
+    f.writelines('DSPRDEGR \n')
+    f.writelines(self.spreading_units + '\n')
+    f.writelines('NaN' + '\n')
+
+
+    for it,t in enumerate(self.t):
+        f.writelines("{:%Y%m%d.%H%M%S}\n".format(self.t[it]))
+        for ix,x in enumerate(self.x):
+            f.writelines('LOCATION {:d} \n'.format(ix+1))
+            for ifr,fr in enumerate(self.f):
+                f.writelines('{:f} {:} {:f} \n'.format(self.energy[it,ix,ifr],self.direction[it,ix,ifr],self.spreading[it,ix,ifr]))
+                
+    return
+
+if __name__ == '__main__':
+
+    import os.path
+    
+    OK = []
+
+## TEST: SWAN 1D test files: different xy and directional cooordinate systems
+   
+    file  = r'./testdata/llcdirafreq1.spc'
+    f  = open(file)
+    T = from_file1D(f,source=os.path.basename(file))
+    f.close()
+    print(T,T.Hm0())
+    T.plot(file + '.png')
+    OK.append(np.abs(T.Hm0()[0,0]-1))
+
+    file2 = r'./testdata/llcdirafreq1copy.spc'
+    f2 = open(file2,'w')
+    to_file1D(T,f2)
+    f2.close()
+    
+    file = r'./testdata/xyndirrfreq1.spc'
+    f = open(file)
+    T = from_file1D(f,source=os.path.basename(file))
+    f.close()
+    print(T,T.Hm0())    
+    T.plot(file + '.png')
+    OK.append(np.abs(T.Hm0()[0,0]-1))
+    
+    file2 = r'./testdata/xyndirrfreq1copy.spc'
+    f2 = open(file2,'w')
+    to_file1D(T,f2)
+    f2.close()    
+    
+## TEST: SWAN 2D test files
+
+    file = r'./testdata/xyndirrfreq2.spc'
+    f = open(file)    
+    T = from_file2D(f,source=os.path.basename(file))
+    f.close()
+    print(T,T.energy.shape, T.Hm0())
+    T.plot(file + '.png')
+    OK.append(np.abs(T.Hm0()[0,0]-1))
+    
+## Wrap up units tests    
+    
+    if np.all(np.asarray(OK) < 1e-3):
+        print('OK')
+    else:
+        raise ValueError('Not all unit tests OK')
+    
